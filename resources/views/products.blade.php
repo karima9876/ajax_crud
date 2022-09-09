87fa22294694d05896d028ee4399e00ce2daf17e
Karima Jaman Moon <karimajaman9876@gmail.com>
2022-09-09 12:54:55 +0600

    add product

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
@@ -16,6 +16,7 @@
             <div class="col-md-2"></div>
             <div class="col-md-8">
                 <h2 class="my-5 text-center">Laravel 9 Ajax Crud</h2>
+                <a href="" class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#addModal">Add Product</a>
                 <div class="table-data">
                     <table class="table table-bordered">
                         <thead>
@@ -45,6 +46,7 @@
         
     </div>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @include('add_product_modal')
+    @include('product_js')
   </body>
 </html>
